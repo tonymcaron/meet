cfe8f224a1b7e92ebb28b178bd3dbb73caf23851
tonymcaron <tonymcaron@gmail.com>
2025-09-26 14:51:36 -0700

    Fixed minor typos / syntax issues, handler.js and serverless.yml

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -6,7 +6,7 @@ const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
 const redirect_uris = ["https://meet-phi-bay.vercel.app/"];
 
-const oAuth2Client = new google.auth.oAuth2(
+const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
   CLIENT_SECRET,
   redirect_uris[0]
