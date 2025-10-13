c405511a26810165037dc161d9356d33f623d97b
tonymcaron <tonymcaron@gmail.com>
2025-10-13 16:46:10 -0700

    Update backticks in api.js

--- src/api.js
+++ src/api.js
@@ -18,7 +18,7 @@ export const extractLocations = (events) => {
 
 const checkToken = async (accessToken) => {
   const response = await fetch(
-    'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}'
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
   );
   const result = await response.json();
   return result;
