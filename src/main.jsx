d9b92ae80fa1132fd76ecde220aedd59a0320f8b
tonymcaron <tonymcaron@gmail.com>
2025-10-21 17:25:48 -0700

    Add Atatus

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import * as atatus from 'atatus-spa';
+
+atatus.config('054e818e7eb74df2985333399407a07b').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+atatus.notify(new Error('Test Atatus Setup'));
