727d928311ac1ff3231a328eeee809d2d24c2282
tonymcaron <tonymcaron@gmail.com>
2025-10-28 09:39:57 -0700

    Implement PWA code

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import * as atatus from 'atatus-spa';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 atatus.config('054e818e7eb74df2985333399407a07b').install();
 
@@ -10,6 +11,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
-
-atatus.notify(new Error('Test Atatus Setup'));
+);
+serviceWorkerRegistration.register();
